0b4eead69538329e1d7f2290cb2422240c298f8c
Brian Bancroft <hello@brianbancroft.ca>
2016-07-03 13:34:35 -0400

    geocoding no longer results in 400 errors for some addresses

--- geocode_request.py
+++ geocode_request.py
@@ -1,29 +1,13 @@
 from urllib2 import Request, urlopen, URLError
 
-def removeWhiteSpacesAndQuotes(element):
-    if element[0] == " ":
-        element = element[1:]
-
-    # yes, I have to do this twice.
-    if element[0] == " ":
-        element = element[1:]
-
-    if element[len(element) - 1] == " ":
-        element = element[:-1]
-    if element[len(element) - 1] == '"':
-        element = element[:-1]
-    return element
 
 def googleRequest(addressArr):
     apikey1 = "&key=AIzaSyCNtzDEyqEfwGPFiK_ZHfMmAndsFZNzxbw"
     baseURL = "https://maps.googleapis.com/maps/api/geocode/json?address="
-    address = addressArr[0][1:-1].replace(" ","%20").lower
-    index = 1
+    address = ""
 
-    while index < len(addressArr):
-        element = removeWhiteSpacesAndQuotes(addressArr[index])
-        address = address + "+" + element
-        index += 1
+    for element in addressArr:
+        address = address + ",%20" + element.strip().strip('"').lower().replace(" ","%20")
 
     fullURL = baseURL + address + apikey1
 
@@ -32,6 +16,9 @@ def googleRequest(addressArr):
         request = Request(fullURL)
         response = urlopen(request)
         location = response.read()
+        print address + " was succesful..?"
         return location;
     except URLError, e:
         print 'No kittez. Got an error code:', e
+        with open('failure_geocode.txt', 'r+') as out_file:
+            out_file.write("Entry: Address: " + address + ", error: " + e + "\n")
